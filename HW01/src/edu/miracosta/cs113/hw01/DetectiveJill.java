e4e0757af05f4ea5d5b07e07da3eab0811354f9f
Angel Tapia <angeltapia07084759@gmail.com>
2017-01-28 05:01:34 -0800

    Homework01 commit 4
    DetectiveJill class has been defined.
    this class have variable that represent
    assistant jack and variables that represents
    list of weapons, locations, and person(murders)
    involved in the crime.
    Also includes methods to figure out the correct
    theory of detective Jill.till empty)

--- HW01/src/edu/miracosta/cs113/hw01/DetectiveJill.java
+++ HW01/src/edu/miracosta/cs113/hw01/DetectiveJill.java
@@ -1,70 +1,243 @@
-/**
- *
- * @author
- * @version 1.0
- * 
- */
 package edu.miracosta.cs113.hw01;
 
 import java.util.Random ;
-import java.util.Scanner ;
 
 public class DetectiveJill
 {
+	/*
+	 * Instance Variable represent assistant Jack and each
+	 * theory item lists that will be discarded.
+	 */
+	private AssistantJack assistantJack ;
+	private Integer[]  weapons ;  
+	private Integer[]  locations ;
+	private Integer[]  persons ;	
 	
-	private boolean discardedMurder ;
-	private boolean discardedLocation ;
-	private boolean discardedWeapon ;
-	
-	
-	
-	public static void main(String[] args)
+	/** 
+	 * Default constructor is not used in this program, but might be 
+	 * useful for some programmers.
+	 */
+	public DetectiveJill()
 	{
-		Scanner userIn = new Scanner(System.in) ;
-		boolean keepTesting = true ;// Testing
-		while (keepTesting)// Testing: loop until desire testing
-		{
-
-			int answerSet, solution, murder, weapon, location ;
-			//Scanner keyboard = new Scanner(System.in) ;
-			Theory answer = null ;
-			AssistantJack jack ;
-
-			System.out.print("Check theory (1, 2, 3[random]): ") ;
-			answerSet = userIn.nextInt() ;
-			//keyboard.close() ;
-
-			jack = new AssistantJack(answerSet) ;
-
-			Random random = new Random() ;
-			do
-			{
-				murder = random.nextInt(6) + 1 ;
-				location = random.nextInt(10) + 1 ;
-				weapon = random.nextInt(6) + 1 ;
-				solution = jack.checkAnswer(weapon, location, murder) ;
-			}
-			while (solution != 0) ;
-			answer = new Theory(weapon, location, murder) ;
-			System.out.println("Total Checks = " + jack.getTimesAsked() + ", Solution = " + answer) ;
-
-			if (jack.getTimesAsked() > 20)
-			{
-				System.out.println("FAILED!! You're a horrible Detective...") ;
-			}
-			else
-			{
-				System.out.println("WOW! You might as well be called Batman!") ;
-			}
-			
-			// Testing: Option to test again
-			System.out.println("Keep Testing? (y/n)") ;
-			keepTesting = userIn.next().toLowerCase().charAt(0) != 'n' ;			
-		}// Testing: End of testing loop
-		userIn.close() ;// Testing: closing keyboard
-		System.out.println("End of the program!") ;// Testing: End of test
+		assistantJack = null ;
+		weapons   = null ;
+		locations = null ;
+		persons   = null ;		
+	}
+	
+	/**
+	 * Full constructor initializes all of the instance variables.
+	 *  
+	 * @param answerSet initialized assistantJack and is an 
+	 * integer that represents the answer sets:
+	 * 		(1) fixed answer 1, 1, 1
+	 * 		(2) fixed answer 6, 10, 6
+	 * 		(3) Integers randomly assigned
+	 */
+	public DetectiveJill(int answerSet)
+	{	
+		assistantJack    = new AssistantJack(answerSet) ;
+		weapons   = new Integer[TheoryItem.TOTAL_WEAPONS] ;
+		locations = new Integer[TheoryItem.TOTAL_LOCATIONS] ;
+		persons   = new Integer[TheoryItem.TOTAL_MURDERS] ;	
+	}
+	
+	/**
+	 * Mutator for assistantJack (This might be useful for some programmers.)
+	 * 
+	 * @param assistantJack represents assistant Jack.
+	 */
+	public void setAssistantJack(AssistantJack assistantJack)
+	{
+		this.assistantJack = assistantJack ;
+	}
+	
+	/**
+	 * Accessor for assistantJack (This might be useful for some programmers.)
+	 * 
+	 * @return a shallow copy of assistantJack.
+	 */
+	public AssistantJack getAssistantJack()
+	{
+		return assistantJack ;
+	}
+	
+	/**
+	 * Mutator for weapons (This might be useful for some programmers.)
+	 * 
+	 * @param weapons represents the list of weapons involved in the crime.
+	 */
+	public void setWeapons(Integer[] weapons)
+	{
+		this.weapons = weapons ;
+	}	
+	
+	/**
+	 * Accessor for weapons (This might be useful for some programmers.)
+	 * 
+	 * @return a shallow copy of weapons.
+	 */
+	public Integer[] getWeapons()
+	{
+		return weapons ;
+	}
+	
+	/**
+	 * Mutator for locations (This might be useful for some programmers.)
+	 * 
+	 * @param locations represents the list of locations involved in the crime.
+	 */
+	public void setLocations(Integer[] locations)
+	{
+		this.locations = locations ;
 	}
 		
+	/**
+	 * Accessor for locations (This might be useful for some programmers.)
+	 * 
+	 * @return a shallow copy of locations.
+	 */
+	public Integer[] getLocations()
+	{
+		return locations ;
+	}
+
+	/**
+	 * Mutator for persons (This might be useful for some programmers.)
+	 * 
+	 * @param persons represents the list of persons involved in the crime.
+	 */
+	public void setPersons(Integer[] persons)
+	{
+		this.persons = persons ;
+	}	
+
+	/**
+	 * Accessor for persons (This might be useful for some programmers.)
+	 * 
+	 * @return a shallow copy of persons.
+	 */
+	public Integer[] getPersons()
+	{
+		return persons ;
+	}
 	
+	/**
+	 * getValidTheoryItem randomly looks for item that has not been
+	 * checked off from the list provide in the parameter.
+	 * (If the element is null, then the item in the list
+	 * 	has not been checked off.)
+	 * 
+	 * @param theoryItems represents any of the list of items.
+	 * 
+	 * @return an item number in the list suggested parameter.
+	 */
+	public int getValidTheoryItem(Integer[] theoryItems)
+	{
+		Random random = new Random() ;		
+		int item = random.nextInt(theoryItems.length) + 1 ;
+		
+		while (theoryItems[item - 1] != null)
+		{
+			item = random.nextInt(theoryItems.length) + 1 ;
+		}
+		return item ;
+	}
 	
+	/**
+	 * discardTheoryItem can be used to checks off items in the list of,
+	 * weapons, locations, and persons(murders).
+	 * 
+	 * @param item represents (1) weapons, (2) locations, and (3) persons(murders).
+	 * @param weapon is the index of the actual weapon in the weapons list.
+	 * @param location is the index of the actual location in the locations list.
+	 * @param person is the index of the actual person in the persons list.
+	 */
+	public void discardTheoryItem(int item, int weapon, int location, int person)
+	{
+		switch (item)
+		{
+			case 1:
+			{
+				weapons[weapon - 1] = 0 ;
+				break ;
+			}
+				
+			case 2:
+			{
+				locations[location - 1] = 0 ;
+				break ;
+			}
+			
+			case 3:
+			{
+				persons[person - 1] = 0 ;
+				break ;
+			}
+
+			default:
+			{
+				break ;
+			}
+		}
+	}
+		
+	/**
+	 * getCorrectTheory uses a do-while to try as many solution
+	 * as possible in oder to figure out the correct theory.
+	 * Each cycle represents each guess from Detective Jill.
+	 * In order to find the solution, the method getValidTheoryItem
+	 * and discardTheoryItem are used to skip any item that has
+	 * been already checked off.
+	 * 
+	 * @return
+	 */
+	public Theory getCorrectTheory()
+	{
+		int weapon, location, person, solution ;
+		do
+		{
+			weapon = getValidTheoryItem(weapons) ;
+			location = getValidTheoryItem(locations) ;
+			person = getValidTheoryItem(persons) ;
+			solution = assistantJack.checkAnswer(weapon, location, person) ;
+			discardTheoryItem(solution, weapon, location, person) ;
+		}
+		while (solution != 0) ;
+		
+		return new Theory(weapon, location, person) ;
+	}
+	
+	/**
+	 * toString displays objects values
+	 * 
+	 * @return the different items involved in the crime.
+	 */
+	@Override
+	public String toString()
+	{
+		return "In this crime there are " + weapons.length + " different weapons, "
+				+ locations.length + " different locations, and " + persons.length
+				+ " diferent persons.";
+	}
+	
+	/**
+	 * Equals method checks if another object of the class DetectiveJill
+	 * are equals in values.
+	 * 
+	 * @return true if every values are equals.
+	 */
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (obj == null || this.getClass() != obj.getClass())
+		{
+			return false ;
+		}
+		DetectiveJill other = (DetectiveJill) obj ;
+		return this.assistantJack == other.assistantJack
+			&& this.weapons == other.weapons 
+			&& this.persons == other.persons
+			&& this.locations == other.locations ;
+	}	
 }
