e87b76180648bf3cfffa24d9bc0caa41ffcc4d86
Angel Tapia <angeltapia07084759@gmail.com>
2017-04-08 02:01:18 -0700

    Lab09 and Lab10 commit
    Contains:
    *PDF of binary trees drawings of  Exercises for section 6.1 on page 303.
    
    *text file that shows the 3
    different types to traverse a binary tree(preorder, inorder, and
    postorder)

---
+++ Lab09/src/edu/miracosta/cs113/lab09/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains PDF of binary trees drawings of 
+ * Exercises for section 6.1 on page 303.
+ */
+/**
+ * @author Angel Tapia <angelTapia07084759@gmail.com>
+ */
+package edu.miracosta.cs113.lab09 ;
---
+++ Lab10/src/edu/miracosta/cs113/lab10/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains a text file that shows the 3
+ * different types to traverse a binary tree(preorder, inorder, and postorder)
+ */
+/**
+ * @author Angel Tapia <angelTapia07084759@gmail.com>
+ */
+package edu.miracosta.cs113.lab10 ;
